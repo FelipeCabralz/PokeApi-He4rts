47015cfc9073445284583a7bfe5080cd075e0fdb
felipe <felipesbcabral@gmail.com>
2021-10-03 19:49:59 -0300

    Conexão com banco de dados

---
+++ PokeApi.Domain/Treinadores.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeApi.Domain
+{
+    public class Treinadores
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Regioes { get; set; }
+        public int Idade { get; set; }
+    }
+}
---
+++ PokeApi.Repository/PokeApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PokeApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeApi.Repository
+{
+    public class PokeApiContext : IdentityDbContext
+    {
+        public PokeApiContext(DbContextOptions<PokeApiContext> options) : base(options) { }
+
+        public DbSet<Treinadores> Treinadores { get; set; }
+    }
+}
